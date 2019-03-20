8e87b028289c15cdc8e10ee3245a6033f7fd3488
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2019-03-21 03:17:30 +0900

    add documentation for blink

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -16,25 +16,31 @@ fn main() -> ! {
     let p = stm32f103::Peripherals::take().unwrap();
     p.RCC.apb2enr.write(|w| {
         w
+            .iopaen().set_bit()
+            .iopben().set_bit()
             .iopcen().set_bit()
+    });
+    p.GPIOA.crh.write(|w| unsafe { w
+        .mode8().bits(gpio::Mode::Output2MHz.bits())
+        .cnf8().bits(gpio::OutputCnf::Pushpull.bits())
+    });
+    p.GPIOB.crl.write(|w| unsafe { w
+        .mode5().bits(gpio::Mode::Input.bits())
+        .cnf5().bits(gpio::InputCnf::PullUpdown.bits())
     });
     p.GPIOC.crh.write(|w| unsafe { w
         .mode13().bits(gpio::Mode::Output2MHz.bits())
         .cnf13().bits(gpio::OutputCnf::Opendrain.bits())
     });
 
+    p.GPIOA.odr.write(|w| {
+        w.odr8().bit(true)
+    });
+
     loop {
+        let bit = p.GPIOB.idr.read().idr5().bit();
         p.GPIOC.odr.write(|w| {
-            w.odr13().bit(true)
+            w.odr13().bit(bit)
         });
-        for _ in 0..80000 {
-            cortex_m::asm::nop();
-        }
-        p.GPIOC.odr.write(|w| {
-            w.odr13().bit(false)
-        });
-        for _ in 0..80000 {
-            cortex_m::asm::nop();
-        }
     }
 }
